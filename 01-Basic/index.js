071817c654848be381dad72d0f24989358e428d4
Piyush289kumar <piyushraikwar289@gmail.com>
2024-06-04 21:10:24 +0530

    Use Middleware in Redux called "redux-logger"

--- 01-Basic/index.js
+++ 01-Basic/index.js
@@ -1,6 +1,7 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
 // Store
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(logger.default));
 
 const history = [];
 
