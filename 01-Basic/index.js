e32947ba056f35913cefa1d66a5df21c8989f537
Piyush289kumar <piyushraikwar289@gmail.com>
2024-06-04 20:48:16 +0530

    "Added Redux store and reducer to index.js, and updated package.json to include 'type': 'module'"

---
+++ 01-Basic/index.js
@@ -0,0 +1,9 @@
+import { createStore } from "redux";
+
+const store = createStore(reducer);
+
+function reducer(state = { amount: 1 }, action) {
+  return state;
+}
+
+console.log(store.getState());
