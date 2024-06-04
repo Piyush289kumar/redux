ff4c21d2b6c0f63abb3ec27ea39d0132e7b70f9c
Piyush289kumar <piyushraikwar289@gmail.com>
2024-06-04 20:55:04 +0530

    Change Global State

--- 01-Basic/index.js
+++ 01-Basic/index.js
@@ -1,9 +1,19 @@
 import { createStore } from "redux";
-
+// Store
 const store = createStore(reducer);
-
+// Reducer
 function reducer(state = { amount: 1 }, action) {
+  if (action.type === "add") {
+    return { state: state.amount + 1 };
+  }
+
   return state;
 }
+// Global Store
+console.log(store.getState());
 
+// Change Global State
+store.dispatch({ type: "add" });
+
+console.log(store.getState());
 console.log(store.getState());
