f148c46e25a8a21d152977852014ec6ac01bdabc
Piyush289kumar <piyushraikwar289@gmail.com>
2024-06-06 08:31:39 +0530

    "Extracted action types to constants, created action creators, and updated reducer and dispatch calls to use them."

--- 01-Basic/index.js
+++ 01-Basic/index.js
@@ -1,5 +1,10 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
+
+// Constants
+const ADD_ITEM = "add";
+const REMOVE_ITEM = "remove";
+const ADD_ITEM_BY_AMT = "addByAmount";
 // Store
 const store = createStore(reducer, applyMiddleware(logger.default));
 
@@ -7,16 +12,15 @@ const history = [];
 
 // Reducer
 function reducer(state = { amount: 0 }, action) {
-  if (action.type === "add") {
-    // state.amount = state.amount + 1;
+  if (action.type === ADD_ITEM) {
     return { amount: state.amount + 1 };
   }
 
-  if (action.type === "remove") {
+  if (action.type === REMOVE_ITEM) {
     return { amount: state.amount - 1 };
   }
 
-  if (action.type === "addByAmount") {
+  if (action.type === ADD_ITEM_BY_AMT) {
     return { amount: state.amount + action.payload };
   }
 
@@ -29,6 +33,20 @@ store.subscribe(() => {
 });
 
 // Change Global State
+
+// Action Creaters
+
+function add() {
+  return { type: ADD_ITEM };
+}
+function remove() {
+  return { type: REMOVE_ITEM };
+}
+
+function addByAmount(val = 2) {
+  return { type: ADD_ITEM_BY_AMT, payload: val };
+}
+
 setInterval(() => {
-  store.dispatch({ type: "addByAmount", payload: 10 });
+  store.dispatch(addByAmount(10));
 }, 2000);
